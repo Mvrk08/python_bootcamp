fe6b8c3ea461b13df01b7a63524be2dd33df266e
mrmahi2003 <maahirchauhan20@icloud.com>
2026-08-14 23:42:18 +0530

    Initial commit

---
+++ pyhton_basics.py
@@ -0,0 +1,34 @@
+PI = 3.14159
+radius= float(input("enter the radius:  "))
+circumference = 2*PI*radius
+
+final_ans = f"The circumference is {circumference}"
+print(final_ans)
+
+# perimeter  of square
+
+side_1 = int(input("enter the side 1:  cm "))
+side_2 = int(input("enter the side 2:   cm"))
+
+ans= side_1 *side_2
+
+print(f"the perimeter of square is {ans}  cm ")
+
+## batting avg
+
+input_1 =int(input("enter no of runs scored by batsman:  ")
+input_2 =int(input("enter no of times  batsman got out:   "))
+
+batting_avg =  input_1/input_2
+
+print(f"this is the batting average of the batsman: {batting_avg}")
+
+
+
+
+
+
+
+
+
+
